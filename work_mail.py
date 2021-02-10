3dbcce62b2ad05653f0047265b2632d1549a7656
matsutaira <shimpei.hatori@gmail.com>
2021-02-10 09:20:22 +0900

    fixed executing error.

--- work_mail.py
+++ work_mail.py
@@ -1,13 +1,10 @@
-#ライブラリの読み込み
+#モジュールの追加
 import tkinter
-import win32com.client
+from tkinter import messagebox
+from win32com import client
 import configparser
 import datetime
-import win32gui, win32con
-
-#pyinstaller の --noconsole が上手く動かないので以下でコンソール画面を非表示にする
-hide = win32gui.GetForegroundWindow()
-win32gui.ShowWindow(hide , win32con.SW_HIDE)
+import re
 
 #configparseの宣言とiniファイルの読み込み
 config_ini = configparser.ConfigParser()
@@ -22,25 +19,16 @@ def rdo_input():
     #UserID入力ボックスの配置
     frame_user = tkinter.Frame(tki)
     frame_user.pack()
-    label_user = tkinter.Label(frame_user, text='【ユーザID】')
+    label_user = tkinter.Label(frame_user, text='【ユーザID】', width=9)
     label_user.pack(side='left')
     userbox = tkinter.Entry(frame_user, width=80)
     userbox.insert(tkinter.END, config_ini['INFO']['user'])
     userbox.pack(side='left')
-
-    #名前入力ボックスの配置
-    frame_name = tkinter.Frame(tki)
-    frame_name.pack()
-    label_name = tkinter.Label(frame_name, text='【名前】')
-    label_name.pack(side='left')
-    namebox = tkinter.Entry(frame_name, width=80)
-    namebox.insert(tkinter.END, config_ini['INFO']['name'])
-    namebox.pack(side='left')
-
+    
     #Toアドレス入力ボックスの配置
     frame_to = tkinter.Frame(tki)
     frame_to.pack()
-    label_to = tkinter.Label(frame_to, text='【To】')
+    label_to = tkinter.Label(frame_to, text='【To】', width=9)
     label_to.pack(side='left')
     tobox = tkinter.Entry(frame_to, width=80)
     tobox.insert(tkinter.END, config_ini['ADDRESS']['to'])
@@ -49,7 +37,7 @@ def rdo_input():
     #Ccアドレス入力ボックスの配置
     frame_cc = tkinter.Frame(tki)
     frame_cc.pack()
-    label_cc = tkinter.Label(frame_cc, text='【Cc】')
+    label_cc = tkinter.Label(frame_cc, text='【Cc】', width=9)
     label_cc.pack(side='left')
     ccbox = tkinter.Entry(frame_cc, width=80)
     ccbox.insert(tkinter.END, config_ini['ADDRESS']['cc'])
@@ -58,20 +46,29 @@ def rdo_input():
     #Bccアドレス入力ボックスの配置
     frame_bcc = tkinter.Frame(tki)
     frame_bcc.pack()
-    label_bcc = tkinter.Label(frame_bcc, text='【Bcc】')
+    label_bcc = tkinter.Label(frame_bcc, text='【Bcc】', width=9)
     label_bcc.pack(side='left')
     bccbox = tkinter.Entry(frame_bcc, width=80)
     bccbox.insert(tkinter.END, config_ini['ADDRESS']['bcc'])
     bccbox.pack(side='left')
-
+    
     #宛名入力ボックスの配置
     frame_addname = tkinter.Frame(tki)
     frame_addname.pack()
-    label_addname = tkinter.Label(frame_addname, text='【宛名】')
+    label_addname = tkinter.Label(frame_addname, text='【宛名】', width=9)
     label_addname.pack(side='left')
     addnamebox = tkinter.Entry(frame_addname, width=80)
     addnamebox.insert(tkinter.END, config_ini['ADDRESS']['addname'])
     addnamebox.pack(side='left')
+
+    #名前入力ボックスの配置
+    frame_name = tkinter.Frame(tki)
+    frame_name.pack()
+    label_name = tkinter.Label(frame_name, text='【名前】', width=9)
+    label_name.pack(side='left')
+    namebox = tkinter.Entry(frame_name, width=80)
+    namebox.insert(tkinter.END, config_ini['INFO']['name'])
+    namebox.pack(side='left')
 
     #ラジオボタンのラベルをリスト化
     rdo_txt = ['在宅勤務開始', '在宅勤務終了']
@@ -88,20 +85,25 @@ def rdo_input():
         rdo.pack(side='left')
     
     #時刻入力ボックスの配置
-    frame2 = tkinter.Frame(tki)
-    frame2.pack()
-    label2 = tkinter.Label(frame2, text='【業務時間帯】')
-    label2.pack(side='left')
-    timebox = tkinter.Entry(frame2)
-    timebox.insert(tkinter.END, config_ini['WORK']['time'])
-    timebox.pack(side='left')
+    frame_time = tkinter.Frame(tki)
+    frame_time.pack()
+    label_time1 = tkinter.Label(frame_time, text='【業務時間帯】')
+    label_time1.pack(side='left')
+    start_timebox = tkinter.Entry(frame_time)
+    start_timebox.insert(tkinter.END, config_ini['WORK']['start_time'])
+    start_timebox.pack(side='left')
+    label_time2 = tkinter.Label(frame_time, text='～')
+    label_time2.pack(side='left')
+    finish_timebox = tkinter.Entry(frame_time)
+    finish_timebox.insert(tkinter.END, config_ini['WORK']['finish_time'])
+    finish_timebox.pack(side='left')
 
     #業務内容ボックスの配置
     frame3 = tkinter.Frame(tki)
     frame3.pack()
-    label3 = tkinter.Label(frame3, text='【業務内容】')
+    label3 = tkinter.Label(frame3, text='【業務内容】', width=9)
     label3.pack(side='left')
-    jobbox = tkinter.Text(frame3, width=80)
+    jobbox = tkinter.Text(frame3, width=68)
     jobbox.insert(tkinter.END, config_ini['WORK']['contents'])
     jobbox.pack(side='left')
         
@@ -110,12 +112,13 @@ def rdo_input():
         nonlocal result
         result = rdo_var.get()
         config_ini['INFO']['user'] = userbox.get()
-        config_ini['INFO']['name'] = namebox.get()
         config_ini['ADDRESS']['to'] = tobox.get()
         config_ini['ADDRESS']['cc'] = ccbox.get()
         config_ini['ADDRESS']['bcc'] = bccbox.get()
         config_ini['ADDRESS']['addname'] = addnamebox.get()
-        config_ini['WORK']['time'] = timebox.get()
+        config_ini['INFO']['name'] = namebox.get()
+        config_ini['WORK']['start_time'] = start_timebox.get()
+        config_ini['WORK']['finish_time'] = finish_timebox.get()
         config_ini['WORK']['contents'] = jobbox.get('1.0', 'end -1c')
         tki.destroy()
     
@@ -136,15 +139,12 @@ if subject == 'NotSelected':
 #メールを作成
 else:
     #Outlookのオブジェクト設定
-    outlook = win32com.client.Dispatch('Outlook.Application')
+    outlook = client.Dispatch('Outlook.Application')
     mymail = outlook.CreateItem(0)
     
     #datetimeのフォーマット設定
     today = datetime.date.today().strftime('%#m/%#d')
-    
-    #署名
-    sign = config_ini['INFO']['name']
-    
+        
     #メールの作成
     mymail.BodyFormat = 1
     mymail.To = config_ini['ADDRESS']['to']
@@ -154,13 +154,18 @@ else:
 
     #メール本文の作成
     body1 = config_ini['ADDRESS']['addname'] + '\n\n' + config_ini['DEFAULT']['body1'] + '\n' + config_ini['INFO']['name'] + 'です。' + '\n\n'
-    body2 = '\n\n' + config_ini['DEFAULT']['body2'] + '\n' + config_ini['WORK']['time'] + '\n\n'+ config_ini['DEFAULT']['body3'] + '\n' + config_ini['WORK']['contents'] + '\n\n'+ config_ini['DEFAULT']['body4'] + '\n\n' + sign
-
-    if subject == '在宅勤務開始':
-        mymail.Body = body1 + config_ini['START']['body_start'] + body2
     
-    if subject ==  '在宅勤務終了':
-        mymail.Body = body1 + config_ini['FINISH']['body_finish'] + body2
+    if re.fullmatch('^([1-9]|[01][0-9]|2[0-3]):[0-5][0-9]～([1-9]|[01][0-9]|2[0-3]):[0-5][0-9]$', config_ini['WORK']['start_time'] + '～' + config_ini['WORK']['finish_time']) == None:
+        messagebox.showerror('エラー', '時刻が正しく入力されていません。')
+        exit()
+    else:
+        body2 = '\n\n' + config_ini['DEFAULT']['body2'] + '\n' + config_ini['WORK']['start_time'] + '～' + config_ini['WORK']['finish_time'] + '\n\n'+ config_ini['DEFAULT']['body3'] + '\n' + config_ini['WORK']['contents'] + '\n\n'+ config_ini['DEFAULT']['body4']
+        
+        if subject == '在宅勤務開始':
+            mymail.Body = body1 + config_ini['START']['body_start'] + body2
+        
+        if subject ==  '在宅勤務終了':
+            mymail.Body = body1 + config_ini['FINISH']['body_finish'] + body2
     
     #出来上がったメール確認
     mymail.Display(True)
@@ -176,7 +181,11 @@ else:
     config_update.set('ADDRESS', 'cc', config_ini['ADDRESS']['cc'])
     config_update.set('ADDRESS', 'bcc', config_ini['ADDRESS']['bcc'])
     config_update.set('ADDRESS', 'addname', config_ini['ADDRESS']['addname'])
-    config_update.set('WORK', 'time', config_ini['WORK']['time'])
+    config_update.set('WORK', 'start_time', config_ini['WORK']['start_time'])
+    config_update.set('WORK', 'finish_time', config_ini['WORK']['finish_time'])
     config_update.set('WORK', 'contents', config_ini['WORK']['contents'])
     with open('./config.ini', 'w', encoding='utf-8') as f:
         config_update.write(f)
+
+if __name__ == '__main__':
+    rdo_input()
